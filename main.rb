08df9384ec8a661b38bc21af9a62a11989cc1155
catherine turra <cathturra@hotmail.com>
2020-10-21 15:40:19 +1100

    initial commit

--- main.rb
+++ main.rb
@@ -1,20 +1,20 @@
      
 require 'sinatra'
 require 'sinatra/reloader' if development?
-require 'pg'
 
-also_reload File.expand_path(__dir__, 'models/*') if development?
+
+# also_reload File.expand_path(__dir__, 'models/*') if development?
 
 get '/' do
   erb :index
 end
 
-def run_sql
-  db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'travel_app'})
-  result = db.exec_params(sql, params)
-  db.close
-  return result
-end
+# def run_sql
+#   db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'travel_app'})
+#   result = db.exec_params(sql, params)
+#   db.close
+#   return result
+# end
 
 
 
